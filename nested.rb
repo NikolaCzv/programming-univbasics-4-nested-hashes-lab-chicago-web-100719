2015256921ea1306b9b24a5ce95d6002e84cac82
Nikola Raicic <nikola.raicic@hotmail.com>
2019-09-28 20:09:31 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,7 +84,7 @@ def adding_matz
         }
      }
 
-    programmer_hash += [:adding_matz.keys][:yukihiro_matsumoto]
+    programmer_hash = [:adding_matz.keys][:yukihiro_matsumoto]
 end
 
 def changing_alan
